1a7be29e25c71c166efacfe632db9276c649b297
Maxim-Filimonov <tpaktopsp@gmail.com>
2014-08-24 13:10:02 +1000

    refactor specs

--- spec/image_processor/batch_processor_spec.rb
+++ spec/image_processor/batch_processor_spec.rb
@@ -6,9 +6,9 @@ describe ImageProcessor::BatchProcessor do
       file_parser = instance_double('ImageProcessor::FileParser')
       subject = described_class.new(parser: file_parser)
       works = [
-        instance_double('ImageProcessor::Work', name: 'canon1', make: 'CANON', model: ''),
-        instance_double('ImageProcessor::Work', name: 'canon2', make: 'CANON', model: ''),
-        instance_double('ImageProcessor::Work', name: 'nikon1', make: 'NIKON', model: ''),
+        instance_double('ImageProcessor::Work', make: 'CANON', model: ''),
+        instance_double('ImageProcessor::Work', make: 'CANON', model: ''),
+        instance_double('ImageProcessor::Work', make: 'NIKON', model: ''),
       ]
 
       allow(file_parser).to receive(:parse).and_return(works)
@@ -19,20 +19,20 @@ describe ImageProcessor::BatchProcessor do
       expect(result.makes['NIKON'].all_models.length).to eq(1)
     end
 
-    it 'groups work by model' do
+    it 'groups work by model in make' do
       file_parser = instance_double('ImageProcessor::FileParser')
       subject = described_class.new(parser: file_parser)
       works = [
-        instance_double('ImageProcessor::Work', name: 'canon1', model: 'Canon EOS 20D', make: 'canon'),
-        instance_double('ImageProcessor::Work', name: 'canon2', model: 'Canon EOS 20D', make: 'canon'),
-        instance_double('ImageProcessor::Work', name: 'canon3', model: 'Canon EOS 70D', make: 'canon'),
+        instance_double('ImageProcessor::Work', model: 'canon 1', make: 'canon'),
+        instance_double('ImageProcessor::Work', model: 'canon 1', make: 'canon'),
+        instance_double('ImageProcessor::Work', model: 'canon 2', make: 'canon'),
       ]
 
       allow(file_parser).to receive(:parse).and_return(works)
 
       result = subject.process
-      expect(result.makes['canon'].models['Canon EOS 20D'].length).to eq(2)
-      expect(result.makes['canon'].models['Canon EOS 70D'].length).to eq(1)
+      expect(result.makes['canon'].models['canon 1'].length).to eq(2)
+      expect(result.makes['canon'].models['canon 2'].length).to eq(1)
     end
   end
 end
